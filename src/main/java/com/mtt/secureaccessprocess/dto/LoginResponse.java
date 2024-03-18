6a5ce28c9cb26c4cfeeb453bab5eb71180c02b32
TESSOH TENE <michel.tessohtene@cgi.com>
2024-03-18 10:59:10 +0100

    Changing Cors configuration and LoginResponse dto

--- src/main/java/com/mtt/secureaccessprocess/dto/LoginResponse.java
+++ src/main/java/com/mtt/secureaccessprocess/dto/LoginResponse.java
@@ -1,13 +1,4 @@
 package com.mtt.secureaccessprocess.dto;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-public class LoginResponse {
-
-    @JsonProperty("jwt")
-    private String jwt;
-
-    public LoginResponse(String jwt) {
-        this.jwt = jwt;
-    }
+public record LoginResponse(String jwt) {
 }
